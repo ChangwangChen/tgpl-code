79e0dc9563cf80a38c5aafce5fc4d0ff3fed5612
changwang <changwang1104@163.com>
2020-02-01 19:58:42 +0800

    chapter5

---
+++ chapter5/defer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x) //panic if x == 0
+	defer func() {fmt.Printf("defer %d\n", x)}() //匿名函数
+	f(x - 1) //递归
+}
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
+}
+
+func main() {
+	defer printStack()
+	f(4)
+}
