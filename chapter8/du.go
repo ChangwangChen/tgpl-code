ffe8e162a3e600ceedc6d21cc8bfee7949ac779a
changwang <changwang1104@163.com>
2020-02-09 23:23:02 +0800

    取消 goroutine

--- chapter8/du.go
+++ chapter8/du.go
@@ -12,6 +12,10 @@ import (
 
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
+	//接收到取消信息， 直接返回
+	if cancelled() {
+		return
+	}
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			n.Add(1)
@@ -24,7 +28,12 @@ func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 }
 
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}
+	select {
+	case <-done:
+		return nil
+	case sema <- struct{}{}:
+	}
+
 	defer func() { <-sema }()
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -37,6 +46,16 @@ func dirents(dir string) []os.FileInfo {
 
 var verbose = flag.Bool("v", false, "show verbose progress messages")
 var sema = make(chan struct{}, 20)
+var done = make(chan struct{}) //取消信号的 通道
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -44,6 +63,12 @@ func main() {
 	if len(roots) == 0 {
 		roots = []string{"."} //默认当前目录
 	}
+
+	//启用新 goroutine 监听 StdIn， 接受取消信息
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) //这里比较简陋， 任意的一个输入都当作取消的信息
+		close(done) //关闭 done chan 当作取消的信息
+	}()
 
 	//遍历
 	fileSizes := make(chan int64)
@@ -85,6 +110,11 @@ loop:
 			nbytes += size
 		case <-tick:
 			printDiskUsage(nfiles, nbytes)
+		case <-done:
+			for range fileSizes {
+				//收到取消信号之后， 读取完所有 fileSizes chan 中的信息， 防止阻塞 goroutine
+			}
+			return
 		}
 	}
 	printDiskUsage(nfiles, nbytes)
